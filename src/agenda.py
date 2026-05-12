bc487619aff43ee33ae9a2819269ebfa2c5bd6a9
priscila-conmits <dominguezpriscila2005@gmail.com>
2026-05-11 21:02:52 -0300

    Update agenda.py
    git add src/agenda.py
    git commit -m "Agregar funciones para agregar y ver contactos"
    git push origin feature/agregar-y-ver

--- src/agenda.py
+++ src/agenda.py
@@ -1,5 +1,28 @@
 # src/agenda.py - Agenda de Contactos 
 contactos = []
+def agregar_contacto():
+  nombre = input("Nombre del contacto: "). strip()
+  if nombre == "":
+    print("El nombre no puede estar vacío.")
+    return
+  telefono = input("Telefono: ").strip()
+  if telefono == "":
+    print("El telefono no puede estar vacío.")
+    return
+  contactos.append({"nombre": nombre, "telefono": telefono})
+  print(f"Contacto '{nombre}' agregando correctamente.")
+  def ver_contactos():
+    if len(contactos) == 0:
+      print("\n La agenda esta vacía.")
+      return
+    print("\n LISTA DE CONTACTOS:")
+    print("-" * 30)
+    for i, c in enumerate(contactos, 1):
+      print(f" {i}.  {c['nombre']} - {c['telefono']}")
+    print("-" * 30)
+    print(f"Total: {len(contactos)} contacto(s)")
+
+
 def mostrar_menu():
   print("\n===== AGENDA DE CONTACTOS =====")
   print("1. Agregar contactos")
